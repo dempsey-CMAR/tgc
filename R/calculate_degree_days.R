f147ea289bd972db3038e7f8f82963a43e880e4c
Danielle Dempsey <ddempsey@perennia.ca>
2021-04-15 16:12:02 -0300

    docs: changed argument to dat to be consistent with other functions; re-wrote description of arg so can use inheritParams

--- R/calculate_degree_days.R
+++ R/calculate_degree_days.R
@@ -11,25 +11,24 @@
 #'   Celcius, and so that day is filtered out of the analysis. In the degree-day
 #'   calculation \emph{n} = 13 days.
 #'
-#' @param dat.tidy Data in tidy format, as returned by the function
-#'   \code{convert_to_tidydata()}. Must include three columns: \code{TIMESTAMP}
-#'   (POSIXct), \code{VARIABLE} (character), and \code{VALUE} (numeric). May
-#'   also include columns with grouping variables passed to \code{...}. Other
-#'   columns will be ignored.
+#' @param dat Dataframe with at least three columns: \code{TIMESTAMP} (must be
+#'   possible to convert to a Date object), \code{DEPTH}, and \code{VALUE}. If
+#'   column \code{VARIABLE} is included, it must have one unique entry. May also
+#'   include columns with grouping variables passed to \code{...}. Other columns
+#'   will be ignored.
 #'
 #' @param start.date First day to include in the calculation. If
 #'   \code{start.date} does not include a time, then the start time is assumed
 #'   to be midnight. Accepted orders for \code{start.date} are: "ymd IMS p",
 #'   "Ymd IMS p", "Ymd HM", "Ymd HMS", "dmY HM", "dmY HMS", "Ymd", "ymd".
-#'   Default is the first TIMESTAMP in \code{dat.tidy}.
+#'   Default is the first TIMESTAMP in \code{dat}.
 #' @param end.date Last day to include in the calculation. If \code{end.date}
 #'   does not include a time, then the end time is assumed to be "23:59:59".
 #'   Accepted orders for \code{end.date} are: "ymd IMS p", "Ymd IMS p", "Ymd
 #'   HM", "Ymd HMS", "dmY HM", "dmY HMS", "Ymd", "ymd". Default is the last
-#'   TIMESTAMP in \code{dat.tidy}.
-#' @param ... Columns in \code{dat.tidy} to use for grouping in
-#'   \code{dplyr::group_by()}, e.g., \code{YEAR, MONTH}. Degree-days are
-#'   calculated for each combination of groups.
+#'   TIMESTAMP in \code{dat}.
+#' @param ... Columns in \code{dat} to use for grouping in
+#'   \code{dplyr::group_by()}, e.g., \code{YEAR, DEPTH}.
 #' @return Returns a tibble with at least five columns: \code{PERIOD} (start and
 #'   end date used to calculate mean temperature and number of days),
 #'   \code{n_DAYS} (the number of days, \emph{n}, used in the calculation),
@@ -44,11 +43,24 @@
 #' @import dplyr
 #' @importFrom lubridate date parse_date_time
 #' @export
+#'
+#' @examples
+#' data(string_data)
+#'
+#' # degree-days averaged over all depths and the whole time series
+#' calculate_degree_days(string_data)
+#'
+#' # degree-days by DEPTH for whole time series
+#' calculate_degree_days(string_data, DEPTH)
+#'
+#' # degree-days by DEPTH and month from July 1 to September 30
+#' calculate_degree_days(string_data, DEPTH, months(TIMESTAMP),
+#' start.date = "2019-07-01", end.date = "2019-09-30")
 
-calculate_degree_days <- function(dat.tidy,
+calculate_degree_days <- function(dat,
                                   ...,
-                                  start.date = min(dat.tidy$TIMESTAMP),
-                                  end.date = max(dat.tidy$TIMESTAMP)
+                                  start.date = min(dat$TIMESTAMP),
+                                  end.date = max(dat$TIMESTAMP)
                                   ){
 
   # format dates (convert to datetime object and add time if only date was supplied)
@@ -56,13 +68,13 @@ calculate_degree_days <- function(dat.tidy,
   end.date <- format_date_for_dd(end.date, day1 = FALSE)
 
   # filter to the dates of interest
-  dat.tidy <- dat.tidy %>%
+  dat <- dat %>%
     filter(VARIABLE == "Temperature",
            TIMESTAMP >= start.date,
            TIMESTAMP <= end.date)
 
   # calculate and return degree-days
-  dat.tidy %>%
+  dat %>%
     # group by the columns specified in ...
     group_by(...) %>%
     mutate(DATE = date(TIMESTAMP)) %>%
