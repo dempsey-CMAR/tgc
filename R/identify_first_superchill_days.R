782b88edc9afd985f0760bbb8c8a8ccd0e4e5598
Danielle Dempsey <ddempsey@perennia.ca>
2021-04-21 08:25:42 -0300

    fix: modify to select first time value goes below threshold

--- R/identify_first_superchill_days.R
+++ R/identify_first_superchill_days.R
@@ -1,7 +1,22 @@
+#' Identifies when VALUE first crosses below a lower threshold
 
+#' @details Identifies timestamp when value first crosses below threshold for
+#'   each group in \code{...}.
+#'
+#' @inheritParams calculate_degree_days
+#' @param superchill_threshold Default is \code{lower_threshold = -0.7}.
+#' @return Returns the TIMESTAMP (for each group in \code{...}) for the first
+#'   time VALUE goes below \code{superchill_threshold}.
+#'
+#'   No row will be returned for groups for which VALUE did not cross
+#'   \code{superchill_threshold}.
+#'
+#' @importFrom dplyr arrange mutate filter summarise ungroup
+#' @importFrom lubridate year
+#' @export
+#
 
-identify_first_superchill_days <- function(dat, lower_threshold = -0.7, ...){
-
+identify_first_superchill_days <- function(dat, ..., superchill_threshold = -0.7){
 
   if("VARIABLE" %in% colnames(dat)){
 
@@ -18,10 +33,11 @@ identify_first_superchill_days <- function(dat, lower_threshold = -0.7, ...){
     group_by(...) %>%
     arrange(TIMESTAMP, .by_group = TRUE) %>%
     mutate(CROSS_THRESH = if_else(
-      lag(VALUE) < lower_threshold & VALUE >= lower_threshold, TRUE, FALSE )
+      lag(VALUE) > superchill_threshold &
+        VALUE <= superchill_threshold, TRUE, FALSE )
     ) %>%
     filter(CROSS_THRESH) %>%
-    summarise(START_TREND = min(TIMESTAMP)) %>%
+    summarise(FIRST_CROSS = min(TIMESTAMP)) %>%
     ungroup()
 
 }
