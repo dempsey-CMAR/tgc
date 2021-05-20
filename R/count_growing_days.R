10d1b23ea4aad6bbc263a9d2ef5060c3de892785
Danielle Dempsey <ddempsey@perennia.ca>
2021-05-20 14:34:34 -0300

    fix: will now group by SEASON

--- R/count_growing_days.R
+++ R/count_growing_days.R
@@ -1,7 +1,9 @@
 #' Count number of days that were suitable for growth
 #'
-#' @inheritParams identify_heat_stress_events
+#' @inheritParams apply_dd_filters
+#'
 #' @param ... Additional grouping variables (beside \code{DEPTH}).
+#'
 #' @return Returns a tibble with columns: DEPTH, START_SEASON (minimum TIMESTAMP
 #'   for each group), END_SEASON (maximum TIMESTAMP for each group), TOTAL_DAYS
 #'   \code{difftime(END_SEASON, START_SEASON, units = "days")}, n_filtered_days
@@ -19,11 +21,15 @@
 
 count_growing_days <- function(dat,
                                ...,
+                               trend_threshold = 4,
+                               superchill_threshold = -0.7,
+                               max_season = 18,
                                heat_threshold = 18,
                                n_hours = 24){
 
   # number of days filtered out due to heat stress events
   filtered_days <- identify_heat_stress_events(dat = dat,
+                                               ...,
                                                heat_threshold = heat_threshold,
                                                n_hours = n_hours) %>%
     mutate(
@@ -31,12 +37,26 @@ count_growing_days <- function(dat,
         difftime(stress_end, stress_start, units = "day")
       )
     ) %>%
-    group_by(DEPTH, ...) %>%
+    group_by(..., DEPTH) %>%
     summarize(n_filtered_days = sum(n_filtered_days))
 
 
- dat %>%
-    group_by(DEPTH, ...) %>%
+  if("STATION" %in% colnames(dat)){
+
+    dat_out <- dat %>%
+      st_filter_growing_seasons(trend_threshold = trend_threshold,
+                                superchill_threshold = superchill_threshold,
+                                max_season = max_season)
+  } else{
+
+    dat_out <- dat %>%
+      filter_growing_seasons(trend_threshold = trend_threshold,
+                             superchill_threshold = superchill_threshold,
+                             max_season = max_season)
+  }
+
+  dat_out %>%
+    group_by(..., SEASON, DEPTH) %>%
     summarise(
       START_SEASON = min(TIMESTAMP),
       END_SEASON = max(TIMESTAMP)
