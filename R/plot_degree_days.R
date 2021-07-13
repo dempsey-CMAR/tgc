32df9572ccb9fa5f497af196b926c28212ec755f
Danielle Dempsey <ddempsey@perennia.ca>
2021-07-13 09:41:58 -0300

    initial commit

---
+++ R/plot_degree_days.R
@@ -0,0 +1,62 @@
+#' Plot degree days at depth for each season start date
+#'
+#' @inheritParams plot_temperature_at_depth
+#'
+#' @param dd_table Dataframe with degree-day data, as exported from
+#'   \code{count_degree_days}. Must include columns \code{DEPTH},
+#'   \code{START_SEASON}, \code{n_degree_days}.
+#'
+#' @return ggplot object
+#'
+#' @import ggplot2
+#' @importFrom stats as.formula
+#' @importFrom strings convert_depth_to_ordered_factor get_colour_palette
+#'   get_xaxis_breaks
+#'
+#' @export
+#'
+
+plot_degree_days <- function(dd_table,
+                             facet_var = NULL,
+                             ncol = 1,
+                             nrow = NULL){
+
+
+  dd_table <- dd_table %>%
+    strings::convert_depth_to_ordered_factor()
+
+  col_pal <- strings::get_colour_palette(dd_table)
+
+  p <- ggplot(dd_table, aes(x = START_SEASON, y = n_degree_days)) +
+    geom_point(shape = 21, col = "darkgrey", aes(fill = DEPTH)) +
+    geom_line(aes(col = DEPTH), show.legend = FALSE) +
+    scale_fill_manual(name = "Depth", values = col_pal) +
+    scale_colour_manual(values = col_pal) +
+    guides(fill = guide_legend(override.aes = list(size = 4))) +
+    theme_light()
+
+  if(is.null(facet_var)){
+
+    axis.breaks <- strings::get_xaxis_breaks(dat)
+
+    p <- p +
+      scale_x_datetime(
+        breaks = axis.breaks$date.breaks.major,
+        minor_breaks = axis.breaks$date.breaks.minor,
+        date_labels =  axis.breaks$date.labels.format
+      )
+  }
+
+
+  if(is.character(facet_var))  {
+
+    facet_var <- as.formula(paste("~", facet_var))
+
+    p <- p +
+      facet_wrap(facet_var, ncol = ncol, nrow = nrow)
+  }
+
+  p
+}
+
+
