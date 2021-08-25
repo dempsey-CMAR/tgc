9d0d6555fa87aab32af2acb60104831e6209d31c
Danielle Dempsey <ddempsey@perennia.ca>
2021-08-25 15:18:45 -0300

    remove redundant set.seed()

--- tests/testthat/test_heat_stress_functions.R
+++ tests/testthat/test_heat_stress_functions.R
@@ -4,12 +4,30 @@ library(dplyr)
 library(tgc)
 
 
+# Set up test data where TIMESTAMP of exceedences is specified ------------
+
 # 15-minute TIMESTAMP for 1 month
-TIMESTAMP <- tibble(
-  TIMESTAMP = seq(
-    as_datetime("2021-07-01"), as_datetime("2021-07-31"), by = "15 min"
-  )
-)
+TIMESTAMP <- tibble(TIMESTAMP = seq(as_datetime("2021-07-01"),
+                                    as_datetime("2021-07-31"),
+                                    by = "15 min"))
+
+# TIMESTAMP for exceedences for each STATION and DEPTH
+set.seed(189)
+exceed1 <- sample(TIMESTAMP$TIMESTAMP, size = 10)
+# add some overlapping intervals
+set.seed(189)
+exceed1.1 <- sample(exceed1, size = 5) + hours(12)
+exceed1 <- c(exceed1, exceed1.1)
+
+exceed2 <- sample(TIMESTAMP$TIMESTAMP, 10)
+
+exceed3 <- sample(TIMESTAMP$TIMESTAMP, 10)
+# add some overlapping intervals
+exceed3.1 <- sort(exceed3)[1:5] + hours(12)
+exceed3 <- c(exceed3, exceed3.1)
+
+exceed4 <- sample(TIMESTAMP$TIMESTAMP, 10)
+
 
 # fake data for 2 STATIONs, 2 DEPTHs each, with all values < 18
 set.seed(1809)
@@ -26,33 +44,8 @@ dat <- TIMESTAMP %>%
   rbind(
     TIMESTAMP %>%
       mutate(STATION = "Station2", DEPTH = 15, VALUE = rnorm(n(), 5, 2))
-  )
-
-max(dat$VALUE)
-
-# TIMESTAMP for exceedances for each STATION and DEPTH
-set.seed(189)
-exceed1 <- sample(TIMESTAMP$TIMESTAMP, size = 10)
-# add some overlapping intervals
-set.seed(189)
-exceed1.1 <- sample(exceed1, size = 5) + hours(12)
-exceed1 <- c(exceed1, exceed1.1)
-
-set.seed(18)
-exceed2 <- sample(TIMESTAMP$TIMESTAMP, 10)
-
-set.seed(4769)
-exceed3 <- sample(TIMESTAMP$TIMESTAMP, 10)
-# add some overlapping intervals
-set.seed(4769)
-exceed3.1 <- sort(exceed3)[1:5] + hours(12)
-exceed3 <- c(exceed3, exceed3.1)
-
-set.seed(76)
-exceed4 <- sample(TIMESTAMP$TIMESTAMP, 10)
-
-# add exceedences
-dat <- dat %>%
+  ) %>%
+  # add exceedences
   mutate(
     VALUE = case_when(
       STATION == "Station1" & DEPTH == 2 & TIMESTAMP %in% exceed1 ~ 18,
@@ -64,6 +57,11 @@ dat <- dat %>%
       TRUE ~ VALUE)
   )
 
+# shuffle to make sure order does not matter
+set.seed(468)
+rows <- sample(nrow(dat))
+dat <- dat[rows, ]
+
 plot_temperature_at_depth(dat, facet_var = "STATION + DEPTH")
 
 
@@ -73,20 +71,20 @@ heat_stress_int <- identify_heat_stress_intervals(dat, STATION)  %>%
   mutate(DIFF = difftime(interval_end, interval_start, unit = "hour"))
 
 # exceedences identified by function for each STATION and DEPTH
-check1 <- filter(heat_stress_int, STATION == "Station1", DEPTH == 2)
-check2 <- filter(heat_stress_int, STATION == "Station1", DEPTH == 5)
-check3 <- filter(heat_stress_int, STATION == "Station2", DEPTH == 10)
-check4 <- filter(heat_stress_int, STATION == "Station2", DEPTH == 15)
+check_int1 <- filter(heat_stress_int, STATION == "Station1", DEPTH == 2)
+check_int2 <- filter(heat_stress_int, STATION == "Station1", DEPTH == 5)
+check_int3 <- filter(heat_stress_int, STATION == "Station2", DEPTH == 10)
+check_int4 <- filter(heat_stress_int, STATION == "Station2", DEPTH == 15)
 
 
 # check that all TIMESTAMPs identified by function are in exceed &
 # function identified all TIMESTAMPs from exceed
 test_that("identify_heat_stress_intervals() finds correct interval_start",{
 
-  expect_equal(check1$interval_start, sort(exceed1))
-  expect_equal(check2$interval_start, sort(exceed2))
-  expect_equal(check3$interval_start, sort(exceed3))
-  expect_equal(check4$interval_start, sort(exceed4))
+  expect_equal(check_int1$interval_start, sort(exceed1))
+  expect_equal(check_int2$interval_start, sort(exceed2))
+  expect_equal(check_int3$interval_start, sort(exceed3))
+  expect_equal(check_int4$interval_start, sort(exceed4))
 
 })
 
@@ -105,24 +103,24 @@ heat_stress_events <- dat %>%
   mutate(DIFF = difftime(stress_end, stress_start, unit = "hour"))
 
 # check that stress_start is in exceed and ONLY from exceed
-check1 <- filter(heat_stress_events, STATION == "Station1", DEPTH == 2)
-check2 <- filter(heat_stress_events, STATION == "Station1", DEPTH == 5)
-check3 <- filter(heat_stress_events, STATION == "Station2", DEPTH == 10)
-check4 <- filter(heat_stress_events, STATION == "Station2", DEPTH == 15)
+check_event1 <- filter(heat_stress_events, STATION == "Station1", DEPTH == 2)
+check_event2 <- filter(heat_stress_events, STATION == "Station1", DEPTH == 5)
+check_event3 <- filter(heat_stress_events, STATION == "Station2", DEPTH == 10)
+check_event4 <- filter(heat_stress_events, STATION == "Station2", DEPTH == 15)
 
 test_that("identify_heat_stress_events() finds correct stress_start", {
 
-  expect_true(all(check1$stress_start %in% exceed1))
-  expect_false(any(!(check1$stress_start %in% exceed1)))
+  expect_true(all(check_event1$stress_start %in% exceed1))
+  expect_false(any(!(check_event1$stress_start %in% exceed1)))
 
-  expect_true(all(check2$stress_start %in% exceed2))
-  expect_false(any(!(check2$stress_start %in% exceed2)))
+  expect_true(all(check_event2$stress_start %in% exceed2))
+  expect_false(any(!(check_event2$stress_start %in% exceed2)))
 
-  expect_true(all(check3$stress_start %in% exceed3))
-  expect_false(any(!(check3$stress_start %in% exceed3)))
+  expect_true(all(check_event3$stress_start %in% exceed3))
+  expect_false(any(!(check_event3$stress_start %in% exceed3)))
 
-  expect_true(all(check4$stress_start %in% exceed4))
-  expect_false(any(!(check4$stress_start %in% exceed4)))
+  expect_true(all(check_event4$stress_start %in% exceed4))
+  expect_false(any(!(check_event4$stress_start %in% exceed4)))
 
 })
 
@@ -138,12 +136,12 @@ test_that("heat stress events do not overlap", {
 })
 
 # check that event lengths match what is expected
-test_that("legnth of heat stress events matches expected values", {
+test_that("length of heat stress events matches expected values", {
 
   expect_equal(as.numeric(max(heat_stress_events$DIFF)), 56.5)
   expect_equal(as.numeric(min(heat_stress_events$DIFF)), 24)
   expect_equal(round(as.numeric(mean(heat_stress_events$DIFF)), digits = 2),
-               30.62)
+               33.06)
 
 })
 
@@ -154,32 +152,29 @@ dat_filt <- dat %>% st_filter_out_heat_stress_events()
 
 plot_temperature_at_depth(dat_filt, facet_var = "STATION + DEPTH")
 
-
-# check that obs identified in
 test_that("st_filter_out_heat_stress_events() removes values >= threshold", {
 
-  expect_equal(nrow(dat_filt %>% filter(VALUE >= 18)), 0)
-
+  expect_equal(nrow(filter(dat_filt, VALUE >= 18)), 0)
 
 })
 
-
-check1 <- check1 %>% select(-STATION, -DEPTH, -event_id, -DIFF)
+# heat stress *intervals* for each STATION & DEPTh to use in data.table function
+check1 <- check_int1 %>% select(-STATION, -DEPTH, -DIFF)
 dat1 <- dat_filt %>%
   filter(STATION == "Station1", DEPTH == 2) %>%
   select(-STATION, -DEPTH)
 
-check2 <- check2 %>% select(-STATION, -DEPTH, -event_id, -DIFF)
+check2 <- check_int2 %>% select(-STATION, -DEPTH, -DIFF)
 dat2 <- dat_filt %>%
   filter(STATION == "Station1", DEPTH == 5) %>%
   select(-STATION, -DEPTH)
 
-check3 <- check3 %>% select(-STATION, -DEPTH, -event_id, -DIFF)
+check3 <- check_int3 %>% select(-STATION, -DEPTH, -DIFF)
 dat3 <- dat_filt %>%
   filter(STATION == "Station2", DEPTH == 10) %>%
   select(-STATION, -DEPTH)
 
-check4 <- check4 %>% select(-STATION, -DEPTH, -event_id, -DIFF)
+check4 <- check_int4 %>% select(-STATION, -DEPTH, -DIFF)
 dat4 <- dat_filt %>%
   filter(STATION == "Station2", DEPTH == 15) %>%
   select(-STATION, -DEPTH)
@@ -191,7 +186,6 @@ test_that("st_filter_out_heat_stress_events() removes all heat stress events", {
   expect_equal(nrow(setDT(dat2)[TIMESTAMP %inrange% check2]), 0)
   expect_equal(nrow(setDT(dat3)[TIMESTAMP %inrange% check3]), 0)
   expect_equal(nrow(setDT(dat4)[TIMESTAMP %inrange% check4]), 0)
-
 
 })
 
